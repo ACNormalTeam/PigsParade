3a3ceafb9d963a9da8f1146ae163c637b1c43e34
Pedro Sousa <pdrmcsousa@gmail.com>
2019-10-13 16:55:42 +0100

    factory in progress

--- src/org/academiadecodigo/vimdiesels/pigsParade/FoodFactory.java
+++ src/org/academiadecodigo/vimdiesels/pigsParade/FoodFactory.java
@@ -1,4 +1,18 @@
 package org.academiadecodigo.vimdiesels.pigsParade;
 
+import org.academiadecodigo.vimdiesels.pigsParade.grid.Grid;
+
 public class FoodFactory {
+
+    public static Food getNewFood(Grid grid) {
+
+        int random = (int) (Math.random() * FoodType.values().length);
+        FoodType foodType = FoodType.values()[random];
+        /*
+        switch (foodType) {
+            case APPLE:
+                return
+        }*/
+        return null;
+    }
 }
